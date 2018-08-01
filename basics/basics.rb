004039bda2626b2b4be5a1ffdafb4515b0575bed
Oscar Bellechasse <obellech10@gmail.com>
2018-07-31 22:05:48 -0500

    exercises from basics chapter

---
+++ basics/basics.rb
@@ -0,0 +1,41 @@
+
+movies = {
+  jaws: 1975,
+  step_bros: 2004,
+  some_movie: 2013,
+  space: 2001,
+  war_games: 1981
+}
+
+puts movies[:jaws]
+puts movies[:step_bros]
+puts movies[:some_movie]
+puts movies[:space]
+puts movies[:war_games]
+
+puts
+
+years = [1975, 2004, 2013, 2001, 1981]
+
+puts years[0]
+puts years[1]
+puts years[2]
+puts years[3]
+puts years[4]
+
+puts
+
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts
+
+puts 4.0 * 4.0
+puts 2.0 * 2.0
+puts 1.0 * 1.0
+
+puts
+
+
