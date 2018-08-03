3098203119821066c539bbe999f0acc3b7636aed
Oscar Bellechasse <obellech10@gmail.com>
2018-08-02 21:43:43 -0500

    chapters methods and variables

---
+++ methods/methods.rb
@@ -0,0 +1,60 @@
+
+#mutate
+
+=begin
+a = [1, 2, 3]
+
+def mutate(array)
+  array.pop
+end
+
+p "Before mutate method: #{a}"
+p mutate(a)
+p "After mutate method: #{a}"
+
+#return
+def add_three(number)
+  number + 3
+end
+
+returned_value = add_three(4)
+puts returned_value
+
+
+def add_three(number)
+  return number + 3
+end
+
+returned_value = add_three(4)
+puts returned_value
+
+def add_three(number)
+  return number + 3
+  number + 4
+end
+
+returned_value = add_three(4)
+puts returned_value
+=end
+
+def greeting(name)
+  return puts "Hello, #{name}. How are you today?"
+end
+
+greeting("Oscar")
+
+def multiply(x, y)
+  x * y
+end
+
+puts multiply(2, 3)
+
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
+
+
+
---
+++ variables/variables.rb
@@ -0,0 +1,18 @@
+
+#name.rb
+
+puts "Hello, what is your first name?"
+first_name = gets.chomp
+puts "And your last name, sir?"
+last_name = gets.chomp
+puts "Hello #{first_name + " " + last_name}! It's very nice to meet you."
+10.times {puts first_name}
+
+#age.rb
+puts "How old are you?"
+age = gets.chomp
+
+puts "In 10 years you will be: #{age.to_i + 10}"
+puts "In 20 years you will be: #{age.to_i + 20}"
+puts "In 30 years you will be: #{age.to_i + 30}"
+puts "In 40 years you will be: #{age.to_i + 40}"
