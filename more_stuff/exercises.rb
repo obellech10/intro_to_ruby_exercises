45553911e9f84f85c3024e4484f42e5554aed897
Oscar Bellechasse <obellech10@gmail.com>
2018-08-27 15:14:55 -0500

    more exercises

---
+++ more_stuff/exercises.rb
@@ -0,0 +1,19 @@
+def contains_lab(string)
+  if /lab/.match(string)
+    puts string
+  end
+end
+
+contains_lab("laboratory")
+contains_lab("experiment")
+contains_lab("Pans labyrinth")
+contains_lab("elaborate")
+contains_lab("polar bear")
+
+puts
+
+def execute(&block)
+  block.call
+end
+
+execute {puts "Hello from inside the execute method!"}
